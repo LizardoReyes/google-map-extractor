5f2d2e1a3d25743be9a30abff6f594ff5242ad94
Lizardo Reyes Jara <ciberutilidades@gmail.com>
2025-06-19 14:40:17 -0500

    agregado detector de keyword directo de json

---
+++ detect_keyword.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from partials.helper_csv import leer_json_completo
+from partials.helpers import create_business
+
+
+def detected_keywords(dir_business: Path):
+    # Leemos 1 a cada archivo JSON en el directorio de negocios
+    for file in dir_business.glob("*.json"):
+        raw_data = leer_json_completo(ruta_archivo=file)
+        business = create_business(raw_data)
+        # Imprimimos el nombre del archivo  la clave keywords detectadas
+        print(f"Archivo: {file.name}, Keywords detectadas: {business[0].query.split(' in ')[0]}")
+
+
+def main():
+    BASE_DIR = Path(__file__).resolve().parent
+    DIR_BUSINESSES = BASE_DIR / "businesses"
+
+    detected_keywords(dir_business=DIR_BUSINESSES)
+
+if __name__ == "__main__":
+    main()
